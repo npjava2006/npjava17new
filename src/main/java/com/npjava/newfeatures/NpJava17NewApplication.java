44c90d3b0d3fcd37ca434dd6664852afe152b269
npjava 2006 <30605905+npjava2006@users.noreply.github.com>
2025-02-20 17:49:07 -0500

    initial spring boot parent file .

---
+++ src/main/java/com/npjava/newfeatures/NpJava17NewApplication.java
@@ -0,0 +1,13 @@
+package com.npjava.newfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpJava17NewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NpJava17NewApplication.class, args);
+        System.out.println(" Spring Boot Application Started Successfully!");
+    }
+}
+
