ad37456f50994e04dccda6a459c51f08b1b25b44
npjava 2006 <30605905+npjava2006@users.noreply.github.com>
2025-03-18 08:29:52 -0400

    Enable caching.

--- src/main/java/com/npjava/newfeatures/NpJava17NewApplication.java
+++ src/main/java/com/npjava/newfeatures/NpJava17NewApplication.java
@@ -2,8 +2,10 @@ package com.npjava.newfeatures;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = "com.npjava")
+@EnableCaching
 public class NpJava17NewApplication {
     public static void main(String[] args) {
         SpringApplication.run(NpJava17NewApplication.class, args);
