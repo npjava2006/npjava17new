5bb103cb8549ea9c1e1514f1c438e5d34052c26f
npjava 2006 <30605905+npjava2006@users.noreply.github.com>
2025-10-19 01:34:31 -0400

    separated backend and front end codes with angular and springboot.

---
+++ np-java/src/test/java/com/npjava/newfeatures/controller/InitialControllerTest.java
@@ -0,0 +1,26 @@
+package com.npjava.newfeatures.controller;
+
+import com.npjava.newfeatures.NpJava17NewApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(InitialController.class)
+@SpringBootTest(classes = NpJava17NewApplication.class)
+class InitialControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //@Test
+    void testSayHello() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, npjava2006!"));
+    }
+}
